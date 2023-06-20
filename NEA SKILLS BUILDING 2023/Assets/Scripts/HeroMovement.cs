065c2fc470fa96d800a64a2d5887e0279db72f88
Atharva-Bhonde <bhonde.a01@elliotthudsoncollege.ac.uk>
2023-06-20 10:48:12 +0100

    task 4 finished

--- NEA SKILLS BUILDING 2023/Assets/Scripts/HeroMovement.cs
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/HeroMovement.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class HeroMovement : MonoBehaviour {
 	Rigidbody2D rb;
 	float speed;
+	public int lives;
+	bool vulnerable;
 
 	// Use this for initialization
 	void Start () {
 		speed = 5.1f;
 		rb = GetComponent<Rigidbody2D>();
+		lives = 2;
+		vulnerable = false;
 	}
 	
 	// Update is called once per frame
@@ -24,5 +30,34 @@ public class HeroMovement : MonoBehaviour {
 	void resetPosition(){
 		Debug.Log ("SPIKED!");
 		transform.SetPositionAndRotation (new Vector3 (-5.43f, 0.86f, 0), Quaternion.identity);
+		setLives ();
+	}
+
+	public void setLives(){
+		lives -= 1;
+		if (lives < 0 || vulnerable == true) {
+			Debug.Log ("End of Game");
+			SceneManager.LoadScene ("Lost");	
+		}
+	}
+
+	private void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "extraLife") {
+			lives += 1;
+			Destroy (other.gameObject);
+		}
+		else if (other.gameObject.tag == "vulnerable"){
+			Destroy (other.gameObject);
+			vulnerable = true;
+			Debug.Log ("vulnerable = true!");
+			setLives ();
+			StartCoroutine ("VulnerableDebuff");
+		}
+
+	}
+	IEnumerator VulnerableDebuff(){
+		yield return new WaitForSeconds (5f);
+		vulnerable = false;
+		Debug.Log ("vulnerable = false!");
 	}
 }
--- NEA SKILLS BUILDING 2023/Assets/Scripts/TextController.cs
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/TextController.cs
@@ -5,15 +5,22 @@ using TMPro;
 
 public class TextController : MonoBehaviour {
 	public TextMeshPro gemCounter;
+	public TextMeshPro livesCounter;
 	GameObject[] totalGems;
+	int lives;
+	GameObject Hero;
+	public TextMeshPro gameTimer;
 	// Use this for initialization
 	void Start () {
-		
+		Hero = GameObject.FindGameObjectWithTag ("Player");
 	}
 	
 	// Update is called once per frame
 	public void Update () {
 		totalGems = GameObject.FindGameObjectsWithTag ("redGem");
 		gemCounter.text = "Gems: " + totalGems.Length.ToString ();
+		lives = Hero.GetComponent<HeroMovement>().lives;
+		livesCounter.text = "Lives: " + lives;
+		gameTimer.text = Time.timeSinceLevelLoad.ToString ("0.00");
 	}
 }
