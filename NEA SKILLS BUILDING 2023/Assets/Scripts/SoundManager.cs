edff83be226d260f060f41708653bbad27bf6a1e
Atharva-Bhonde <bhonde.a01@elliotthudsoncollege.ac.uk>
2023-06-27 10:47:52 +0100

    Task 8 finished but bomb and life sounds not playing.

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public AudioClip coin;
+	private AudioSource source;
+	public AudioClip life;
+	public AudioClip bomb;
+	// Use this for initialization
+	void Start () {
+		source = GameObject.FindObjectOfType<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other){
+		Debug.Log ("bing!");
+		source.PlayOneShot (coin, 1);
+	}
+
+	private void OnCollisionEnter2d(Collision2D other)
+	{
+		if (other.gameObject.tag == "extraLife") {
+			source.PlayOneShot (life, 1);
+		} 
+		else if (other.gameObject.tag == "vulnerable") {
+			source.PlayOneShot (bomb, 1);
+		}
+	}
+}
