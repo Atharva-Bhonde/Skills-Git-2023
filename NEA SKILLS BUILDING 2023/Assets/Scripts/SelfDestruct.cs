9507a81d4d219957a431c5197b5266c5f1f04562
Atharva-Bhonde <135663464+Atharva-Bhonde@users.noreply.github.com>
2023-06-29 13:07:19 +0100

    task 12 finished

---
+++ NEA SKILLS BUILDING 2023/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, 10);
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2 (4, 8), ForceMode2D.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnDestroy(){
+		Debug.Log ("A bomb was destroyed");	
+	
+	}
+}
